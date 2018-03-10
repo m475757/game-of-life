706509f740576807c33f55a939e8b7508bb8c1da
Tian <168321015@qq.com>
2018-03-10 10:14:51 +0800

    Update WhenYouCreateACell.java

--- gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/WhenYouCreateACell.java
+++ gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/WhenYouCreateACell.java
@@ -13,6 +13,7 @@ public class WhenYouCreateACell {
     public void aLiveCellShouldBeRepresentedByAnAsterisk() {
         Cell cell = Cell.fromSymbol("*");
         assertThat(cell, is(LIVE_CELL));
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     }
 
     @Test
